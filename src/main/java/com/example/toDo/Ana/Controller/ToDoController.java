de6447eaa12380c96477cf1c99f04c9b185f1c33
ahmet celil yalman <106810170+AhmetCelil@users.noreply.github.com>
2024-07-13 23:50:54 +0300

    main project

---
+++ src/main/java/com/example/toDo/Ana/Controller/ToDoController.java
@@ -0,0 +1,58 @@
+package com.example.toDo.Ana.Controller;
+
+import com.example.toDo.Ana.Entity.ToDo;
+import com.example.toDo.Ana.Service.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //restfull web servisi olduğunu gösterir
+@RequestMapping("/api/to_do") //isteklerin yönlenmesi için
+public class ToDoController {
+    @Autowired
+    private ToDoService todoservice;
+
+
+    /////////////////////////GET ISLEMLERIı
+
+
+
+    @GetMapping // veri tabnında bulunan nesneleri listelemek için kullanılır
+    public List<ToDo> getAlltoDo() {
+        System.out.println("VERİLER LİSTELENDİ...");
+        return todoservice.getAllToDo();
+    }
+    @GetMapping("/{id}")  //id ile çağırma işlemi için kullanılır
+    public ToDo getToDoById(@PathVariable Long id) {
+        return todoservice.getToDoById(id);
+    }
+    @GetMapping("/page")
+    public List<ToDo> getToDosByPage(@RequestParam int page, @RequestParam int size) {
+        return todoservice.getToDosByPage(page, size);
+    }
+
+    ////////////////////////POST ISLEMLERI
+    @PostMapping
+    public ToDo CreateToDo(@RequestBody ToDo todo) {
+        System.out.println("veri eklendi...\nID numarası: "+ todo.getId());
+        return todoservice.saveToDo(todo);
+    }
+
+    ///////////////////////GUNCELLEME ISLEMLERI
+    @PutMapping("/{id}")
+    public ToDo UpdateToDo(@PathVariable Long id, @RequestBody ToDo todo) {
+        return todoservice.saveToDo(todo);
+
+    }
+
+
+    ////////////////////////DELETE ISLEMLERI
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteToDo(@PathVariable Long id) {
+        todoservice.deleteToDoById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
